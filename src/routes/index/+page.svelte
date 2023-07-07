efed24d4e72d2e87826e03dabc5e82a09cccc1bc
Yuzyx17 <braitboat@gmail.com>
2023-07-07 21:24:40 +0800

    change to async instead of onmount

--- src/routes/index/+page.svelte
+++ src/routes/index/+page.svelte
@@ -1,29 +1,38 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    // import { onMount } from "svelte";
+    // onMount(async () => {
+    //     let response = await fetch("/mockdata.json");
+    //     mockdata = await response.json()
+    // })
 
-    let mockdata: any;
-
-    onMount(async () => {
+    async function loadMockData(){
         let response = await fetch("/mockdata.json");
-        mockdata = await response.json()
-    })
+        let mockdata: Promise<any> = await response.json()
+        if(mockdata){
+            return mockdata
+        }else{
+            throw new Error(mockdata)
+        }
+    }
+
+    let mockdata = loadMockData()
 
 </script>
 <div>
-    <!-- {#await mockdata}
+    {#await mockdata}
         <p>Loading...</p>
-    {:then mockdata}
-        {#each mockdata as mock}
-        <p>Name is {mock.EMAIL}</p>
+    {:then mock}
+        {#each mock as item}
+        <p>{item.EMAIL}</p>
         {/each}
     {:catch error}
         <p style="color: red">{error.message}</p>
-    {/await} -->
-    {#if mockdata === undefined}
+    {/await}
+    <!-- {#if mockdata === undefined}
         Loading Character Name...
     {:else}
         {#each mockdata as mock}
             {mock.EMAIL}<br>
         {/each}
-    {/if}   
+    {/if}    -->
 </div>
