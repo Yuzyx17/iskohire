ffe82df0933609027b6c9c26e0d99dd868fdff01
Karmotrine <yuanrcurena@gmail.com>
2023-07-31 08:23:47 +0800

    minor fix on storing role_id

--- src/routes/api/redirect/dashboard/+page.server.ts
+++ src/routes/api/redirect/dashboard/+page.server.ts
@@ -1,3 +1,4 @@
+import { roleid } from '$lib/stores/auth.js'
 import { fail, redirect } from '@sveltejs/kit'
 
 export const load = async ({ locals: { supabase, getSession } }) => {
@@ -18,17 +19,19 @@ export const load = async ({ locals: { supabase, getSession } }) => {
     // Get role from session
     let role: number = profile?.role_id ?? ''
 
+    roleid.set(role)
+
     // if role == student, redirect to student dashboard
     // if role == alumni, redirect to alumni dashboard
     // if role == employer, redirect to employer dashboard
     if (role == 1) {
-        throw redirect(303, '/student/dashboard')
+        throw redirect(303, '/profile')
     } else if (role == 2) {
-        throw redirect(303, '/alumni/dashboard')
+        throw redirect(303, '/alumni-dashboard')
     } else if (role == 3) {
-        throw redirect(303, '/employer/dashboard')
+        throw redirect(303, '/emp-dashboard')
     } else if (role == 4) {
-        throw redirect(303, '/admin/dashboard')
+        throw redirect(303, '/admin')
     }
   
     return { session, profile, error }
